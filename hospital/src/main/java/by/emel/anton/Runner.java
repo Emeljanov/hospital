81f7dafb355493057e54e062fb07f384ccf6c8fe
Emeljanov <anton.yemialyanau@gmail.com>
2021-01-27 14:12:25 +0300

    try ctr

--- hospital/src/main/java/by/emel/anton/Runner.java
+++ hospital/src/main/java/by/emel/anton/Runner.java
@@ -1,25 +1,16 @@
 package by.emel.anton;
 
-import by.emel.anton.model.beans.users.patients.Patient;
-import by.emel.anton.model.dao.implementation.springdatadao.SpringDataPatientDAO;
-import by.emel.anton.model.dao.implementation.springdatadao.SpringDataUserDAO;
-import by.emel.anton.model.dao.implementation.springdatadao.intefaces.PatientJpaRepository;
-import by.emel.anton.model.dao.implementation.springdatadao.intefaces.UserJpaRepository;
-import by.emel.anton.terminalprog.TerminalProgram;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 @SpringBootApplication
 @Transactional
 public class Runner implements CommandLineRunner {
-
-    @Autowired
-    private TerminalProgram terminalProgram;
+//
+//    @Autowired
+//    private TerminalProgram terminalProgram;
 
     public static void main(String[] args) {
 
@@ -29,6 +20,6 @@ public class Runner implements CommandLineRunner {
     @Override
     public void run(String... args) {
 
-        terminalProgram.startProgram();
+//        terminalProgram.startProgram();
     }
 }
