56676e40ce9648ea116d2d58628a83bc186d3776
Emeljanov <anton.yemialyanau@gmail.com>
2021-02-04 16:41:48 +0300

    v0.0.01

--- hospital/src/test/java/by/emel/anton/controller/DoctorControllerTest.java
+++ hospital/src/test/java/by/emel/anton/controller/DoctorControllerTest.java
@@ -1,10 +1,13 @@
 package by.emel.anton.controller;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -23,8 +26,20 @@ public class DoctorControllerTest {
         this.doctorController = doctorController;
     }
 
+    @BeforeEach
+    @Sql
+    public void addToDb() {
+
+    }
+
+    @Test
+    public void testController() {
+        Assertions.assertNotNull(doctorController);
+    }
+
     @Test
     public void getDoctorByLoginPass() {
+
     }
 
     @Test
