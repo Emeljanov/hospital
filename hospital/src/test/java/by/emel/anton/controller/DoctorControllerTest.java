a2a22cab40ed2a578c8cbac25d5b6f2ba32a2569
Emeljanov <anton.yemialyanau@gmail.com>
2021-02-04 14:05:16 +0300

    add app.proper for test

--- hospital/src/test/java/by/emel/anton/controller/DoctorControllerTest.java
+++ hospital/src/test/java/by/emel/anton/controller/DoctorControllerTest.java
@@ -1,14 +1,28 @@
 package by.emel.anton.controller;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-@TestPropertySource(locations = "classpath:test.properties")
-//@AutoConfigureMockMvc(secure = false)
+//@TestPropertySource(locations = "classpath:test.properties")
+@AutoConfigureMockMvc
 public class DoctorControllerTest {
+
+    private DoctorController doctorController;
+    private MockMvc mockMvc;
+
+    @Autowired
+    public DoctorControllerTest(MockMvc mockMvc, DoctorController doctorController) {
+        this.mockMvc = mockMvc;
+        this.doctorController = doctorController;
+    }
+
     @Test
     public void getDoctorByLoginPass() {
     }
